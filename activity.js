f62fe573aebee800905d7782f2df9c4173050bbf
Abhishek Agrwal <abhishekagrwal93@gmail.com>
2024-06-21 16:47:36 +0530

    Added activity recommendations. - set up .env file & env variables. - added nodemon dev dependency - renamed API method.

---
+++ activity.js
@@ -0,0 +1,7 @@
+const activities = {
+    Cloudy: [ 'Baking', 'Gym', 'Reading' ],
+    Rainy: [ 'Table tennis', 'Reading', 'Visit cultural center' ],
+    Sunny: [ 'Cycling', 'Jogging', 'Picknicking' ]
+}
+
+export default activities;
--- app.js
+++ app.js
@@ -1,38 +1,54 @@
 'use strict';
 
-const Koa = require('koa');
-const Router = require('@koa/router');
-const axios = require('axios')
+import Koa from 'koa';
+import KoaRouter from '@koa/router';
+import axios from 'axios';
+import activites from './activity.js'
 
+
+// Instantiation
 const app = new Koa();
-const router = new Router();
+const router = new KoaRouter();
 
-const api_key = 'ce89541b80484bfebca111038242006'
 
-router.get('/weather', async (ctx) => {
+// Route for current weather
+router.get('/currentweather', async (ctx) => {
 
-    const current_weather_api = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${ctx.query.city}`;
+    const current_weather_url = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${ctx.query.city}`;
 
     try{
-        const weather_api_response = await axios.get(current_weather_api);
+        const weather_api_response = await axios.get(current_weather_url);
+        
+        // Categorize / bucket the weather condition
+        const condition = weather_api_response.data.current.condition.text;
+        let condition_category;
 
-        const response = {
+        if (condition.toLowerCase().indexOf('cloud') > -1 || condition.toLowerCase().indexOf('mist') > -1)  {
+            condition_category = 'Cloudy';
+        } else if(condition.toLowerCase().indexOf('rain') > -1 ) {
+            condition_category = 'Rainy';
+        } else if(condition.toLowerCase().indexOf('sun') > -1) {
+            condition_category = 'Sunny';
+        }
+        
+        // Build response from a subset of fields in the response from external API.
+        const filtered_response = {
             city: weather_api_response.data.location.name,
             temp_c: weather_api_response.data.current.temp_c,
             temp_f: weather_api_response.data.current.temp_f,
-            condition: weather_api_response.data.current.condition.text
-        }
+            condition: condition,
+            condition_category: condition_category,
+            recommended_activites: activites[condition_category]
+        };
 
-        ctx.body = response;
-
+        ctx.body = filtered_response;
+        
     } catch(e) {
-        ctx.status = 422;
+        ctx.status = 500;
         ctx.body = e.message;
     }
 });
 
-app
-    .use(router.routes())
-    .use(router.allowedMethods());
 
-app.listen('3000')
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(process.env.PORT || 3000)
